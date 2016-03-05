9af70546717e6cc4ce699bbcc478dbe1518d45f5
Vinh Nguyen <xuanvinhct@gmail.com>
2016-03-05 16:00:54 +0700

    add complex type

--- w001/display.rs
+++ w001/display.rs
@@ -27,14 +27,26 @@ impl fmt::Display for Structure{
     }
 }
 
+#[derive(Debug)]
+struct Complex(i32, i32);
+impl fmt::Display for Complex{
+    // Signature
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f, "(real={},img={})", self.0, self.1)
+    }
+}
+
+
 fn main(){
     let s = Structure(150);
     println!("Custom display: {}", s);
 
     let mimax = MinMax(-300, 400);
     let p = Point(12, 64);
+    let c1 = Complex(12, 22);
     println!("Debug minmax: {0:?}", mimax);
     println!("Display minmax: {}", mimax);
     println!("Debug point: {0:?}", p);
     println!("Display point: {}", p);
+    println!("Display complex: {}", c1);
 }
